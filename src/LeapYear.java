4bd579834824502be1c6d9b3abc107b3e4f87084
Michał Murawski <homasha@gmail.com>
2022-08-08 15:06:05 +0200

    Create isLeapYear method

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,4 +1,10 @@
 public class LeapYear {
     public static void main(String[] args) {
+        int y = 2022;
+        isLeapYear(y);
+    }
+
+    private static void isLeapYear(int y) {
+        System.out.println("Is the leap year: ");
     }
 }
