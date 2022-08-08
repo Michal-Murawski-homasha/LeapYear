83856271c85ac36c1bd794b710d8b2e845ee2a9c
Michał Murawski <homasha@gmail.com>
2022-08-08 15:25:28 +0200

    Counts leap year

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,10 +1,14 @@
 public class LeapYear {
     public static void main(String[] args) {
-        int y = 2022;
+        int y = 2024;
         isLeapYear(y);
     }
 
     private static void isLeapYear(int y) {
-        System.out.println("Is the leap year: ");
+        if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)) {
+            System.out.println(y + " is a leap year");
+        } else {
+            System.out.println(y + " is not leap year");
+        }
     }
 }
