5d9966287c235cd4f27c5cc0b3dedf41d79fee38
Michał Murawski <homasha@gmail.com>
2022-08-08 15:36:00 +0200

    Removes comment

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class LeapYear {
     public static void main(String[] args) {
-//        int y = 2024;
         Scanner in = new Scanner(System.in);
         System.out.println("Press year: ");
         int y = in.nextInt();
